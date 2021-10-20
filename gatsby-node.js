32c47b5942d5491dbfda19a4cea16f8e907c91bc
Juan Jose Cordoba Ortiz <juan.cordoba@pragma.com.co>
2021-10-20 14:16:51 -0500

    first commit

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,197 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/node-apis/
- */
+const path = require(`path`);
 
-// You can delete this file if you're not using it
+const makeRequest = (graphql, request) => new Promise((resolve, reject) => {
+    resolve(
+        graphql(request).then(result => {
+            if(result.errors) {
+                reject(result.errors)
+            }
+            return result;
+        })
+    )
+});
+
+exports.createPages = ({actions, graphql}) => {
+    const { createPage } = actions;
+
+    const getBlog = makeRequest(graphql, `
+        {
+            allContentfulBlog (
+                sort: { fields: [createdAt], order: DESC }
+                filter: {
+                    node_locale: {eq: "en-US"}},)
+                {
+                    edges {
+                        node {
+                            id
+                            slug
+                        }
+                    }
+                }
+        }
+    `).then(result => {
+        result.data.allContentfulBlog.edges.forEach(({ node }) => {
+            createPage({
+                path: `blog/${node.slug}`,
+                component: path.resolve(`src/templates/blog.js`),
+                context: {
+                    id: node.id,
+                },
+            })
+        })
+    });
+
+    // Create archive page for all blogs, including pagination
+    const getArchive = makeRequest(graphql, `
+        {
+            allContentfulBlog (
+                sort: { fields: [createdAt], order: DESC }
+                filter: {
+                    node_locale: {eq: "en-US"}},)
+                {
+                    edges {
+                        node {
+                            id
+                            slug
+                        }
+                    }
+                }
+        }
+    `).then(result => {
+        const blogs = result.data.allContentfulBlog.edges
+        const blogsPerPage = 9
+        const numPages = Math.ceil(blogs.length / blogsPerPage)
+
+        Array.from({ length: numPages }).forEach((_, i) => {
+            createPage({
+                path: i === 0 ? `/blog` : `/blog/${i + 1}`,
+                component: path.resolve(`src/templates/archive.js`),
+                context: {
+                    limit: blogsPerPage,
+                    skip: i * blogsPerPage,
+                    numPages,
+                    currentPage: i + 1
+                },
+            })
+        })
+    });
+    
+    // Create Travel page, including pagination
+    const getTravel = makeRequest(graphql, `
+        {
+            allContentfulBlog (
+                sort: { fields: [createdAt], order: DESC }
+                filter: {
+                    node_locale: {eq: "en-US"}
+                    category: {elemMatch: {title: {eq: "Travel"}}}
+                },)
+                {
+                    edges {
+                        node {
+                            id
+                            slug
+                        }
+                    }
+                }
+        }
+    `).then(result => {
+        const blogs = result.data.allContentfulBlog.edges
+        const blogsPerPage = 9
+        const numPages = Math.ceil(blogs.length / blogsPerPage)
+
+        Array.from({ length: numPages }).forEach((_, i) => {
+            createPage({
+                path: i === 0 ? `/category/travel` : `/category/travel/${i + 1}`,
+                component: path.resolve(`src/templates/travel.js`),
+                context: {
+                    limit: blogsPerPage,
+                    skip: i * blogsPerPage,
+                    numPages,
+                    currentPage: i + 1
+                },
+            })
+        })
+    });
+   
+    // Create Guide page, including pagination
+    const getGuide = makeRequest(graphql, `
+        {
+            allContentfulBlog (
+                sort: { fields: [createdAt], order: DESC }
+                filter: {
+                    node_locale: {eq: "en-US"}
+                    category: {elemMatch: {title: {eq: "Guide"}}}
+                },)
+                {
+                    edges {
+                        node {
+                            id
+                            slug
+                        }
+                    }
+                }
+        }
+    `).then(result => {
+        const blogs = result.data.allContentfulBlog.edges
+        const blogsPerPage = 9
+        const numPages = Math.ceil(blogs.length / blogsPerPage)
+
+        Array.from({ length: numPages }).forEach((_, i) => {
+            createPage({
+                path: i === 0 ? `/category/guide` : `/category/guide/${i + 1}`,
+                component: path.resolve(`src/templates/guide.js`),
+                context: {
+                    limit: blogsPerPage,
+                    skip: i * blogsPerPage,
+                    numPages,
+                    currentPage: i + 1
+                },
+            })
+        })
+    });
+    
+    // Create Tech page, including pagination
+    const getTech = makeRequest(graphql, `
+        {
+            allContentfulBlog (
+                sort: { fields: [createdAt], order: DESC }
+                filter: {
+                    node_locale: {eq: "en-US"}
+                    category: {elemMatch: {title: {eq: "Tech"}}}
+                },)
+                {
+                    edges {
+                        node {
+                            id
+                            slug
+                        }
+                    }
+                }
+        }
+    `).then(result => {
+        const blogs = result.data.allContentfulBlog.edges
+        const blogsPerPage = 9
+        const numPages = Math.ceil(blogs.length / blogsPerPage)
+
+        Array.from({ length: numPages }).forEach((_, i) => {
+            createPage({
+                path: i === 0 ? `/category/tech` : `/category/tech/${i + 1}`,
+                component: path.resolve(`src/templates/tech.js`),
+                context: {
+                    limit: blogsPerPage,
+                    skip: i * blogsPerPage,
+                    numPages,
+                    currentPage: i + 1
+                },
+            })
+        })
+    });
+
+    return Promise.all([
+        getBlog,
+        getArchive,
+        getTravel,
+        getGuide,
+        getTech
+    ])
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,24 @@
 import * as React from "react"
 import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
+import Nav from "../components/nav"
 import Seo from "../components/seo"
+
+import Featured from "../components/featured"
+import Home from "../components/home"
+import Footer from "../components/footer"
+
+import './index.css';
 
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["auto", "webp", "avif"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
+    <Nav />
+    <Featured />
+    <Home />
+    <Link to="/blog" className="viewmore">View More</Link>
+    <Footer />
   </Layout>
 )
 
